8fd8e01d707f87f807efd7339d0b7d9b6c61af7d
Paulo Ribeiro <60105800+pauloribeiro-codacy@users.noreply.github.com>
2021-02-17 11:22:16 +0000

    test: Trigger security issues on GitHub Code scanning (#13)
    * test: Add /crypto directory from WebGoat
    
    * test: Add OWASP Top 10 examples
    
    * test: Add ruleset.xml
    
    * test: Manipulate results.sarif file before uploading to GitHub
    
    * test: Reduce number of test files
    
    * Cleanup repository

--- foo.md
+++
@@ -1,5 +0,0 @@
-This is a sample Markdown file.
-
-- This is a list item.
-*   This is another list item.
-
--- foo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import hashlib
-import random
-import telnetlib
-
-a = hashlib.sha1(str(random.getrandbits(256)))
-
-print(a)
