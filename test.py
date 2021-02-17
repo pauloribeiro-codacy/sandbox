fdc2e282bb1860afee5b21191dbbc04d1831de6b
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2021-02-17 12:44:23 +0000

    test: Trigger severe Bandit issue

--- test.py
+++ test.py
@@ -2,8 +2,12 @@
 
 import hashlib
 import random
+import telnetlib
+import requests
 
 a = hashlib.sha1(str(random.getrandbits(256)))
+
+b = requests.get("https://gmail.com", verify=False)
 
 print(a)
 
